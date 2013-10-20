3b9c57c27c138bec90d67339c82b60629972c389
yalcindo <yalcindo1@gmail.com>
2013-10-20 10:11:27 -0600

    Remainder adeded

--- main.js
+++ main.js
@@ -1,19 +1,17 @@
 
 var getChunk= function(input,num){
     var division=Math.floor(input.length/num);
-    var reminder=input.length%num;
+    var remainder=input.length%num;
     var chunks=[];
-    for(i=0;i<num-reminder;i++)
+    for(i=0;i<num-remainder;i++)
     {
     	var chunk= input.splice((input.length-division));
     	chunks.push(chunk);
     }
-    for(i=0;i<reminder;i++)
+    for(i=0;i<remainder;i++)
     {
     	var chunky= input.splice(input.length-(division+1));
     	chunks.push(chunky);
     }
 return chunks.reverse();
 };
-var input = [1,2,3,4,5,6,7,8,9,10,11,12,13];
-console.log(getChunk(input,3));
